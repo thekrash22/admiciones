52328c61faf9c209a41d46172ed5e59ab45287cd
Jeison Javier Cañas De La Hoz <jcanas@MILAGROSA.LOCAL>
2019-09-23 15:42:23 -0500

    Modificacion del pdf

--- resources/views/pdf/inscripcion.blade.php
+++ resources/views/pdf/inscripcion.blade.php
@@ -13,25 +13,133 @@
     </head>
     <body>
 
-        <table class="table table-hover table-striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Producto</th>
-                    <th>Descripción</th>
-                    <th>Stock</th>
-                </tr>
-            </thead>
-            <tbody>
-
-                <tr>
-                    <td> $product->id </td>
-                    <td> $product->name </td>
-                    <td> $product->description </td>
-                    <td class="text-right"> $product->stock </td>
-                </tr>
-
-            </tbody>
+        <table  class="table table-bordered" >
+            <tr>
+                <td rowspan ="4" style="width:20%; padding-bottom: 0px; padding-top: 0px;"><img src="01_Escudo_Presentacion_color.png" width=150px height=150px /></td>
+                <td rowspan ="4" style="width:50%; padding-bottom: 0px; padding-top: 0px;"><p style= "text-align: center;font-size: small;">INSTITUTO LA MILAGROSA<br>PROCESO DE GESTIÓN ADMISIÓN Y MATRÍCULA </p><p style= "text-align: center;">SOLICITUD DE ADMISIÓN</p></td>
+                <td style="width:30%; padding-bottom: 0px; padding-top: 0px;"><p style="text-align: center;margin-bottom: 0px;" >AMFR01</p></td>
+            <tr>
+                <td style="padding-bottom: 0px; padding-top: 0px;">
+                         <p style="text-align: center;margin-bottom: 0px;" >Versión</p>
+                </td>
+            </tr>
+            <tr>
+            <td style="padding-bottom: 0px; padding-top: 0px;"><p style="text-align: center;margin-bottom: 0px;" >Fecha de aprobación</p><p style="text-align: center;margin-bottom: 0px;" >23-04-19</p>
+                            </td>
+            </tr>
+            <tr>
+            <td style="padding-bottom: 0px; padding-top: 0px;"><p style="text-align: center;margin-bottom: 0px;" >Página 1 de 3</p></td>
+            </tr>
         </table>
+
+
+        <table class="table table-bordered" style="border: 2px solid rgb(2, 2, 2);">
+        
+        <tr>
+            <td colspan="11" style="padding-bottom: 0px; padding-top: 0px;"><p style="text-align: center;margin-bottom: 0px;" >I. DATOS PERSONALES DEL ASPIRANTE</p></td>
+          
+        </tr>
+        <tr>
+            <td colspan="3"style="padding-bottom: 0px;padding-top: 0px;padding-left: 3px;">Apellido Completo:</td>
+            
+            
+            <td colspan="6" style="padding-bottom: 0px; padding-top: 0px;"></td>
+            
+            <td style="padding-bottom: 0px; padding-top: 0px;">Edad</td>
+            <td style="padding-bottom: 0px; padding-top: 0px;"></td>
+        </tr>
+        <tr>
+            <td colspan="3" style="padding-bottom: 0px;padding-top: 0px;padding-left: 3px;">Nombre Completo</td>
+            <td colspan="8" style="padding-bottom: 0px; padding-top: 0px;"></td>
+        </tr>
+        <tr>
+            <td colspan="3" rowspan="2" style="padding-bottom: 0px;padding-top: 10px;padding-left: 0px;">Fecha de nacimiento</td>
+            <td style="padding-bottom: 0px; padding-top: 0px;">Día</td>
+            <td style="padding-bottom: 0px; padding-top: 0px;">Mes</td>
+            <td style="padding-bottom: 0px; padding-top: 0px;">Año</td>
+            <td rowspan="2" style="padding-bottom: 0px; padding-top: 10px;">Lugar:</td>
+            <td colspan="2" rowspan="2" style="padding-bottom: 0px; padding-top: 0px;"></td>
+            <td rowspan="2" style="padding-bottom: 0px; padding-top: px;">Grupo Sanguíneo:</td>
+            <td rowspan="2" style="padding-bottom: 0px; padding-top: 0px;"></td>
+        </tr>
+        <tr>
+            <td colspan="3" style="padding-bottom: 0px; padding-top: 20px;"></td>
+        </tr>
+        <tr>
+            <td colspan="2" rowspan="2" style="padding-bottom: 0px;padding-top: 10px;padding-left: 3px;">Identificación</td>
+            
+            <td colspan="3" style="text-align: center; padding-bottom: 0px; padding-top: 0px;  padding-left: 3px;">Tipo de Documento</td>
+            <td colspan="3" style="text-align: center; padding-bottom: 0px; padding-top: 0px; padding-left: 3px;">Número de Documento</td>    
+            <td colspan="3" style="text-align: center; padding-bottom: 0px; padding-top: 0px;">Expedido en:</td>  
+        </tr>
+        <tr>
+            
+            
+            <td colspan="3" style="padding-bottom: 0px; padding-top: 20px;"></td>
+            <td colspan="3" style="padding-bottom: 0px; padding-top: 0px;"></td>   
+            <td colspan="3" style="padding-bottom: 0px; padding-top: 0px;"></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="padding-bottom: 0px;padding-top: 0px;padding-left: 3px;">Dirección:</td>
+            <td colspan="9" style="padding-bottom: 0px; padding-top: 0px;"></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="padding-bottom: 0px;padding-top: 0px;padding-left: 3px;">Barrio</td>
+            <td colspan="5" style="padding-bottom: 0px; padding-top: 0px;"></td>
+            <td style="padding-bottom: 0px; padding-top: 0px;">Teléfono</td>    
+            <td colspan="3" style="padding-bottom: 0px; padding-top: 0px;"></td>
+        </tr>
+        <tr>
+        <td colspan="4" rowspan="2" style="padding-bottom: 0px;padding-top: 10px;padding-left: 3px;">Identificación</td>
+            <td colspan="4" style="text-align: center; padding-bottom: 0px; padding-top: 0px;">Zona</td>    
+            <td colspan="2" rowspan="2" style="padding-bottom: 0px; padding-top: 10px;">Estrato Donde Vive</td>
+            <td rowspan="2" style="padding-bottom: 0px; padding-top: 0px;"></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="padding-bottom: 0px; padding-top: 0px;">Urbana:</td>
+            <td colspan="2" style="padding-bottom: 0px; padding-top: 0px;">Rural</td>
+            
+        </tr>
+        <tr>
+            <td colspan="11" style="padding-bottom: 0px; padding-top: 20px;"></td>
+        </tr>
+        <tr>
+        <td colspan="4" style="padding-bottom: 0px;padding-top: 0px;padding-left: 3px;">E.P.S del Aspirante:</td>
+            <td colspan="7" style="padding-bottom: 0px; padding-top: 0px;"></td>
+        </tr>
+        <tr>
+            <td colspan="7" rowspan="2" style="text-align: center; padding-bottom: 0px; padding-top: 0px;">Tiene Hermanos en el Colegio:<br>(reñaciónelos a Continuación)</td>
+            
+            <td colspan="2" style="text-align: center; padding-bottom: 0px; padding-top: 0px;">Si:</td>
+            <td colspan="2" style="text-align: center; padding-bottom: 0px; padding-top: 0px;">No:</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="padding-bottom: 0px; padding-top: 20px;"></td>    
+            <td colspan="2" style="padding-bottom: 0px; padding-top: 20px;"></td>
+        </tr>
+        <tr>
+            <td colspan="9" style="text-align: center; padding-bottom: 0px; padding-top: 0px;">Nombres y Apellidos</td>
+            <td colspan="2" style="text-align: center; padding-bottom: 0px; padding-top: 0px;">Grado</td>
+           
+        </tr>
+        <tr>
+            <td colspan="11" style="padding-bottom: 0px; padding-top: 20px;"></td>
+        </tr>
+        <tr>
+        <td colspan="11" style="padding-bottom: 0px; padding-top: 20px;"></td>
+        </tr>
+
+
+        </table>
+
+
+
+
+
+
+
+
+
+
     </body>
 </html>
